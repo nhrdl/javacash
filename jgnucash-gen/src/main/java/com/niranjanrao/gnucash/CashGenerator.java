b4629ef9e37bc33e2ff435151a5d1c1f78f9981c
niranjan <niranjan@fiport.com>
2013-08-05 18:38:46 -0700

    Generating constructors

--- jgnucash-gen/src/main/java/com/niranjanrao/gnucash/CashGenerator.java
+++ jgnucash-gen/src/main/java/com/niranjanrao/gnucash/CashGenerator.java
@@ -2,6 +2,7 @@ package com.niranjanrao.gnucash;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -14,6 +15,7 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import com.sun.codemodel.CodeWriter;
+import com.sun.codemodel.JAssignment;
 import com.sun.codemodel.JClass;
 import com.sun.codemodel.JCodeModel;
 import com.sun.codemodel.JDefinedClass;
@@ -22,6 +24,7 @@ import com.sun.codemodel.JFieldVar;
 import com.sun.codemodel.JMethod;
 import com.sun.codemodel.JMod;
 import com.sun.codemodel.JPackage;
+import com.sun.codemodel.JStatement;
 
 public class CashGenerator extends CashBase {
 
@@ -30,6 +33,7 @@ public class CashGenerator extends CashBase {
 	private final JCodeModel model;
 	HashMap<String, Node> defineElementMap;
 	Map<String, String> countDataMap;
+	HashMap<String, GenData> generationData;
 
 	private static Map<String, String> toMap(final String... mappingPairs) throws Exception {
 		if (0 != mappingPairs.length % 2) {
@@ -45,7 +49,7 @@ public class CashGenerator extends CashBase {
 	public CashGenerator() throws Exception {
 		this.model = new JCodeModel();
 		defineElementMap = new HashMap<>();
-
+		generationData = new HashMap<>();
 		countDataMap = toMap("book", "Book", "commodity", "Commodity", "account", "Account", "transaction", "Transaction", "schedxaction",
 				"ScheduledTransaction", "budget", "Budget", "gnc:GncBillTerm", "BillTerm", "gnc:GncCustomer", "Customer", "gnc:GncEmployee", "Employee",
 				"gnc:GncEntry", "Entry", "gnc:GncInvoice", "Invoice", "gnc:GncJob", "Job", "gnc:GncOrder", "Order", "gnc:GncTaxTable", "TaxTable",
@@ -63,7 +67,7 @@ public class CashGenerator extends CashBase {
 	}
 
 	public void generate(final CodeWriter writer) throws IOException {
-		model.build(writer);
+		writeTo(writer);
 	}
 
 	public void writeTo(final com.sun.codemodel.CodeWriter writer) throws IOException {
@@ -87,6 +91,12 @@ public class CashGenerator extends CashBase {
 
 	private void closeClass(final JDefinedClass _class) {
 
+		final GenData data = generationData.get(_class.fullName());
+
+		final JMethod construct = _class.constructor(JMod.PUBLIC);
+		for (final JStatement expr : data.constructorList) {
+			construct.body().add(expr);
+		}
 	}
 
 	public void processRNGFile() throws Exception {
@@ -117,6 +127,8 @@ public class CashGenerator extends CashBase {
 			return cls;
 		}
 		log.info("Going to create class " + classPath);
+		final GenData data = new GenData();
+		this.generationData.put(classPath, data);
 		cls = model._class(classPath);
 		cls._extends(CashBase.class);
 		generateCounters(cls, classData);
@@ -150,9 +162,12 @@ public class CashGenerator extends CashBase {
 			final Node obj = evaluateXPath("//rng:ref[@name='" + typeName + "']");
 			final String fullPath = getClassPath(typeName, obj);
 			final JDefinedClass type = generateClass(fullPath, obj);
-			final JClass list = model.ref(java.util.List.class).narrow(type);
+			final JClass list = model.ref(ArrayList.class).narrow(type);
 			final JFieldVar field = cls.field(JMod.PRIVATE, list, "_" + typeName.toLowerCase() + "s");
 			method.body()._return(field.invoke("size"));
+
+			final JAssignment newStatement = (JAssignment) field.assign(JExpr._new(list));
+			generationData.get(cls.fullName()).constructorList.add(newStatement);
 		}
 	}
 
---
+++ jgnucash-gen/src/main/java/com/niranjanrao/gnucash/GenData.java
@@ -0,0 +1,12 @@
+package com.niranjanrao.gnucash;
+
+import java.util.ArrayList;
+
+import com.sun.codemodel.JDefinedClass;
+import com.sun.codemodel.JStatement;
+
+public class GenData {
+
+	public JDefinedClass _class;
+	public ArrayList<JStatement> constructorList = new ArrayList<>();
+}
